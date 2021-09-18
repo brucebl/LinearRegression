df506b89fe6b0cac3f48b31c879b62f0ab8ae72b
Bruce <mgobust@gmail.com>
2021-09-17 22:10:25 -0400

    Initial version of LinearRegression and an example main program.

---
+++ LinearRegression.cpp
@@ -0,0 +1,183 @@
+#include "LinearRegression.h"
+
+#include <iostream>
+
+LinearRegression::LinearRegression() {
+    
+}
+
+/**
+ * Calculates the mean squared error for the training data set.
+ */
+double LinearRegression::cost(){
+    double cost{0.0};
+    double hypothesis{0.0};
+    
+    for(unsigned long i = 0; i < trainingVec.size(); i++){
+        hypothesis = 0.0;
+        for(unsigned long j = 0; j < weightVec.size(); j++){
+            hypothesis += weightVec.at(j) * trainingVec.at(i).at(j);        
+        }
+
+        cost += pow(hypothesis - targetVec.at(i), 2);
+        
+    }
+
+    cost = (0.5 * cost) / static_cast<double>(trainingVec.size());
+
+    currentCost = cost;
+
+    return cost;
+}
+
+std::string LinearRegression::dblVectToString(const std::vector<double>& dVec) {
+    std::string vectStr;
+    for(int i = 0; i < dVec.size(); i++){
+        vectStr += std::to_string(dVec.at(i));
+        vectStr += " ";
+    }
+
+    return vectStr;
+}
+
+double LinearRegression::differenceEquation(unsigned long trainingVecIndex){
+    double difference{0.0};
+    double hypothesis{0.0};
+
+    unsigned int numOfFeatures = weightVec.size();
+    
+    for(unsigned int i = 0; i < numOfFeatures; i++){
+        hypothesis += weightVec.at(i) * trainingVec[trainingVecIndex][i];
+        std::cout << "hypo=" << hypothesis << "+=" << weightVec.at(i) << " * " << trainingVec[trainingVecIndex][i] << std::endl;
+    }
+
+    difference = hypothesis - targetVec.at(trainingVecIndex);
+    std::cout << "difference=" << difference << "=" << hypothesis << "-" << targetVec.at(trainingVecIndex) << std::endl;
+    return difference;
+}
+
+double LinearRegression::estimate(std::vector<double> testVec) {
+    double hypothesis{0.0};
+
+    if(testVec.size() == weightVec.size() - 1) {
+        testVec.push_back(1.0);
+    } else{
+        return 1;
+    }
+
+    for(int i = 0; i < testVec.size(); i++){
+        hypothesis += testVec.at(i) * weightVec.at(i);
+    }
+
+    return hypothesis;
+}
+
+double LinearRegression::getCost(){
+    return currentCost;
+}
+
+double LinearRegression::getLearningRate(){
+    return learningRate;
+}
+
+std::vector<double> LinearRegression::getTargetData() {
+    return targetVec;
+}
+
+std::vector<std::vector<double>> LinearRegression::getTrainingData(){
+    return trainingVec;
+}
+
+std::vector<double> LinearRegression::getWeights(){
+    return weightVec;
+}
+
+void LinearRegression::gradientDecent(unsigned int maxSteps){
+    double prevCost{DBL_MAX};
+    double curCost{0.0};
+    curCost = cost();
+
+    for(int i = 0; i < maxSteps; i++){
+        std::cout << "\tSTEP " << i << " Weights: " << std::endl;
+        
+        updateWeights();
+        curCost = cost();
+
+        
+        for(int j = 0; j < weightVec.size(); j++){
+            std::cout << j << ": " << weightVec.at(j) << " ";
+        }
+        std::cout << std::endl;
+        std::cout << "+++prevCost: " << prevCost << " curCost: " << curCost << std::endl;
+
+        if(curCost > prevCost){
+            std::cout << "Current cost higher than previous cost." << std::endl;
+            break;
+        } else{
+            std::cout << "Setting previouse cost to current cost." << std::endl;
+            prevCost = curCost;
+        }
+    }
+}
+
+void LinearRegression::loadTargetData(std::vector<double> target){
+    targetVec = target;
+}
+
+void LinearRegression::loadTrainingData(std::vector<std::vector<double>> trainingSetVec){
+    trainingVec = trainingSetVec;
+
+    // Add an end element of 1 for each training row.
+    for(int i = 0; i < trainingVec.size(); i++){
+        trainingVec.at(i).push_back(1.0);
+    }
+
+    // If weights are not already set then initialize all to random value.
+    if(weightVec.size() != trainingVec.at(0).size()){
+        weightVec.clear();
+        std::random_device randDev;
+        std::default_random_engine generator(randDev());
+        std::uniform_real_distribution<double> uniDist(0.0, 1.0);
+
+        for(int i = 0; i < trainingVec.at(0).size(); i++){
+            double randWeight = uniDist(generator);
+            weightVec.push_back(randWeight);
+            std::cout << "Initial weight " << std::to_string(i) << ": " << std::to_string(randWeight) << std::endl;
+        }
+    }
+}
+
+void LinearRegression::reloadWeights(std::vector<double> weights){
+    weightVec = weights;
+}
+
+void LinearRegression::setLearningRate(double rate){
+    learningRate = rate;
+}
+
+void LinearRegression::updateWeights(){
+    double sumOfDer{0.0};
+    double currWeight{0.0};
+    double newWeight{0.0};
+
+    // For each weight perform gradient descent to update it.
+    for(unsigned int i = 0; i < weightVec.size(); i++){
+        std::cout << "updateWeights(): updating weight " << std::to_string(i) << std::endl;
+        sumOfDer = 0.0;
+        double difEq{0.0};
+        for(unsigned long j = 0; j < trainingVec.size(); j++){
+            difEq = differenceEquation(j);
+            sumOfDer += difEq * trainingVec[j][i];
+            std::cout << "sum of der = " << sumOfDer << "=" << difEq << "*" << trainingVec[j][i] << std::endl;
+        }
+        currWeight = weightVec.at(i);
+        newWeight = weightVec.at(i) - (learningRate * (sumOfDer / static_cast<double>(trainingVec.size())));
+        std::cout << "****weight " << i << " updated to " << newWeight << " from " << currWeight <<  std::endl;
+        weightVec.at(i) = newWeight;   
+    }
+}
+
+
+
+
+
---
+++ main.cpp
@@ -0,0 +1,152 @@
+#include "LinearRegression.h"
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+/*
+*   Example of how to use the LinearRegression class.
+*   Program requires two data files for the training data and
+*   target data. For example, house price estimate based on bedrooms,
+*   bathrooms, and square feet. The target data file rows would be the
+*   sale price for the corresponding row in the training data file.
+*   Training Data File:
+*   3   2   1100
+
+*   Target Data File:
+*   250000
+*/
+int main(){
+    string trainDataFName;
+    string targetDataFName;
+    unsigned int miniBatchSize{100};    //Number of samples to use per weight update.
+    unsigned int numOfIterations{100};   //Number of updates to weights.
+    
+    vector<vector<double>> trainingSetVec;  //Holds training data.
+    vector<double> targetVec;   //Holds target data.
+
+    vector<double> testVect;
+    int bedrooms{0};
+    int baths{0};
+    int squareFeet{0};
+    int price{0};
+
+    cout << "Enter training data file name or T(rainingData.txt): ";
+    cin >> trainDataFName;
+    if(trainDataFName.compare("T") == 0){
+        trainDataFName = "TrainingData.txt";
+    }
+
+    ifstream insTrain{trainDataFName};
+    if(!insTrain){ 
+        cout << "Not able to open data file " << trainDataFName;
+        return 0;
+    }
+
+    cout << "Enter target data file name or T(argetData.txt): ";
+    cin >> targetDataFName;
+    if(targetDataFName.compare("T") == 0){
+        targetDataFName = "TargetData.txt";
+    }
+
+    ifstream insTarget{targetDataFName};
+    if(!insTarget){
+        cout << "Not able to open data file " << targetDataFName;
+        return 0;
+    }
+
+    cout << endl;
+
+    // Read in the training data file and put each row into a vector.
+    trainingSetVec.clear();
+
+    while(!insTrain.eof()) {
+        vector<double> dataRow;
+        
+        if(insTrain >> bedrooms >> baths >> squareFeet) {
+            dataRow.push_back(bedrooms);
+            dataRow.push_back(baths);
+            dataRow.push_back(squareFeet);
+            cout << LinearRegression::dblVectToString(dataRow) << endl;
+            trainingSetVec.push_back(dataRow);
+        } else {
+            break;
+        }
+    }
+
+    // Read in the target data file and put it in a vector.
+    targetVec.clear();
+
+    while(!insTarget.eof()){
+        if(insTarget >> price) {
+            targetVec.push_back(price);
+        } else{
+            break;
+        }
+    }
+    
+    cout << LinearRegression::dblVectToString(targetVec);
+
+    cout << "Training set size " << trainingSetVec.size() << endl;
+    cout << "Target set size " << targetVec.size() << endl;
+    if(trainingSetVec.size() != targetVec.size()){
+        cout << "Training set size is not the same a target set size." << endl;
+    } else {
+        cout << "Training and target data set loaded." << endl;
+    }
+
+    // Create the LinearRegression class.
+    LinearRegression* linReg = new LinearRegression();
+    
+    // Load the training data into the LinearRegression object.
+    linReg->loadTrainingData(trainingSetVec);
+
+    cout << "Size of loaded training data: " << linReg->getTrainingData().size() << endl;
+    for(unsigned int i = 0; i < linReg->getTrainingData().size(); i++) {
+        vector<double> trainRow = linReg->getTrainingData().at(i);
+        cout << LinearRegression::dblVectToString(trainRow) << endl;
+    }
+
+    // Load the target data into the LinearRegression object.
+    linReg->loadTargetData(targetVec);
+
+    cout << "Loaded target data: " << endl;
+
+    for(unsigned int i = 0; i < linReg->getTargetData().size(); i++) {
+        cout << to_string(linReg->getTargetData().at(i)) << endl;
+    }
+
+    cout << "Enter number of iterations to perform for each weight update: ";
+    cin >> numOfIterations;
+
+    // Perform gradien decent on the data.
+    linReg->gradientDecent(numOfIterations);
+
+    cout << "Weights: " << endl;
+    for(int i = 0; i < linReg->getWeights().size(); i++){
+        cout << i << ": " << linReg->getWeights().at(i) << " ";
+    }
+  
+    cout << endl;
+    
+    // Prompt for estimation of sale price for a house.
+    while(true){
+        testVect.clear();
+        cout << "Enter bedrooms: ";
+        cin >> bedrooms;
+        cout << "Enter baths: ";
+        cin >> baths;
+        cout << "Enter square feet: ";
+        cin >> squareFeet;
+
+        testVect.push_back(bedrooms);
+        testVect.push_back(baths);
+        testVect.push_back(squareFeet);
+
+        price = linReg->estimate(testVect);
+
+        cout << "Estimated price " << price << endl;
+    }
+
+    return 0;
+}
